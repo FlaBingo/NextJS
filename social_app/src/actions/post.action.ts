e91af82a08cc9a1ebda08e5f2841330f6fd74f98
Satyam Prajapati <satyamm1402@gmail.com>
2025-06-15 11:57:47 +0530

    Add Shadcn UI Alert Dialog and Date-fns dependencies; enhance PostCard with comment and delete functionality

--- social_app/src/actions/post.action.ts
+++ social_app/src/actions/post.action.ts
@@ -34,6 +34,7 @@ export async function getPosts() {
       include: {
         author: {
           select: {
+            id: true,
             name: true,
             image: true,
             username: true
