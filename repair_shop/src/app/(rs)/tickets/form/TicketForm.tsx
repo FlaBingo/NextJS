743c46a0e4bffefa7545a15ddf82df6a0365fbf3
Satyam Prajapati <satyamm1402@gmail.com>
2025-07-02 21:20:46 +0530

    feat: enhance TicketForm with tech selection and completed checkbox based on user role

--- repair_shop/src/app/(rs)/tickets/form/TicketForm.tsx
+++ repair_shop/src/app/(rs)/tickets/form/TicketForm.tsx
@@ -20,9 +20,21 @@ import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel";
 type Props = {
   customer: SelectCustomerSchemaType;
   ticket?: SelectTicketSchemaType;
+  techs?: {
+    id: string;
+    description: string;
+  }[];
+  isEditable?: boolean;
 };
 
-export default function TicketForm({ customer, ticket }: Props) {
+export default function TicketForm({
+  customer,
+  ticket,
+  techs,
+  isEditable = true,
+}: Props) {
+  const isManager = Array.isArray(techs);
+
   const defaultValues: InsertTicketSchemaType = {
     id: ticket?.id ?? "(New)",
     customerId: ticket?.customerId ?? customer.id,
@@ -59,20 +71,38 @@ export default function TicketForm({ customer, ticket }: Props) {
             <InputWithLabel<InsertTicketSchemaType>
               fieldTitle="Title"
               nameInSchema="title"
+              disabled={!isEditable}
             />
 
-            <InputWithLabel<InsertTicketSchemaType>
-              fieldTitle="Tech"
-              nameInSchema="tech"
-              disabled={true}
-            />
+            {isManager ? (
+              <SelectWithLabel<InsertTicketSchemaType>
+                fieldTitle="Tech ID"
+                nameInSchema="tech"
+                data={[
+                  {
+                    id: "new-ticket@example.com",
+                    description: "new-ticket@example.com",
+                  },
+                  ...techs,
+                ]}
+              />
+            ) : (
+              <InputWithLabel<InsertTicketSchemaType>
+                fieldTitle="Tech"
+                nameInSchema="tech"
+                disabled={true}
+              />
+            )}
 
-            <CheckboxWithLabel<InsertTicketSchemaType> 
-              fieldTitle={"Completed"}  
-              nameInSchema={"completed"}
-              message="Yes"
-            />
-            
+            {ticket?.id ? (
+              <CheckboxWithLabel<InsertTicketSchemaType>
+                fieldTitle={"Completed"}
+                nameInSchema={"completed"}
+                message="Yes"
+                disabled={!isEditable}
+              />
+            ) : null}
+
             <div className="mt-4 space-y-2">
               <h3 className="text-lg">Customer Info</h3>
               <hr className="w-4/5" />
@@ -95,9 +125,11 @@ export default function TicketForm({ customer, ticket }: Props) {
               fieldTitle="Description"
               nameInSchema="description"
               className="h-96"
+              disabled={!isEditable}
             />
 
-            <div className="flex justify-between">
+            {isEditable ? (
+              <div className="flex justify-between">
               <Button
                 type="submit"
                 className="w-3/4"
@@ -115,6 +147,9 @@ export default function TicketForm({ customer, ticket }: Props) {
                 Reset
               </Button>
             </div>
+            ) : null}
+
+            
           </div>
         </form>
       </Form>
